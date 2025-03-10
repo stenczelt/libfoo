e9fa80dee1a29cf04aa0f842aede54ed2f503cea
Tamas K Stenczel <tks32@cam.ac.uk>
2025-03-10 07:06:30 +0000

    example added

---
+++ examples/main_example.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Tamas K Stenczel on 10/03/2025.
+//
+
+#include "MathFunctions.h"
+#include <iostream>
+
+int main() {
+    auto val = mathfunctions::sqrt(2.0);
+    std::cout << "sqrt2: " << val << std::endl;
+}

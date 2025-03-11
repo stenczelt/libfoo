320a900c7958bd460b31367ad07623a758690c14
Tamas K Stenczel <tks32@cam.ac.uk>
2025-03-11 08:07:04 +0000

    bare bones mace namespace

---
+++ include/mace/mace.hpp
@@ -0,0 +1,8 @@
+#ifndef MACE_MAIN_HPP
+#define MACE_MAIN_HPP
+
+namespace mace {
+void hello();
+}
+
+#endif // MACE_MAIN_HPP
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "mace/mace.hpp"
+#include <iostream>
+
+namespace mace {
+void hello() {
+  std::cout << "Hello from mace::main!" << std::endl;
+}
+}
